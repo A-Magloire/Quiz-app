2b2cbb76311baf0f49130c451a2a3909699932fc
Adam <94177926+okdemsky@users.noreply.github.com>
2022-01-18 17:10:08 -0500

    Add additional functionality

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ const quizData = [
         b: '23',
         c: '27',
         d: '19',
-        correct: 'c'
+        correct: 'b'
 
     },
     {
@@ -45,7 +45,11 @@ const quizData = [
 
     }
 ];
-
+//get entire quiz
+const quiz = document.getElementById("quiz");
+//get radio answers
+const answersEls = document.querySelectorAll(".answer");
+//get questions and answer text
 const questionEl = document.getElementById('question')
 const a_text = document.getElementById("a_text");
 const b_text = document.getElementById("b_text");
@@ -54,12 +58,14 @@ const d_text = document.getElementById("d_text");
 const submitBtn = document.getElementById("submit")
 
 let currentQuiz = 0;
-
+let score = 0;
 loadQuiz();
 
 function loadQuiz() {
     //assign the quiz data to the question elements defined in HTML
     const currentQuizData = quizData[currentQuiz];
+    //clear radio buttons
+    deselectAnswers();
 
     questionEl.innerText = currentQuizData.question;
     a_text.innerText = currentQuizData.a;
@@ -68,13 +74,40 @@ function loadQuiz() {
     d_text.innerText = currentQuizData.d;
 }
 
-submitBtn.addEventListener("click", () => {
-    currentQuiz++;
+function getSelected() {
+    //get answer values (selected or deselected)
+    let answer = undefined;
 
-    if (currentQuiz < quizData.length) {
-        loadQuiz();
-    } else {
-        alert('Goodjob, You finished!')
+    answersEls.forEach((answerEl) => {
+        if (answerEl.checked) {
+            answer = answerEl.id;
+        }
+    });
+    return answer;
+}
+
+function deselectAnswers() {
+    answersEls.forEach((answerEl) => {
+        answerEl.checked = false
+        });
+ }
+
+submitBtn.addEventListener("click", () => {
+    //check to see the answer
+    const answer = getSelected();
+
+    if(answer) {
+
+        if(answer === quizData[currentQuiz].correct) {
+            score++
+        }
+
+        currentQuiz++;
+        if (currentQuiz < quizData.length) {
+            loadQuiz();
+        } else {
+            quiz.innerHTML = `<h2>You answered correctly at ${score}/${quizData.length} questions.</h2>`
+        }
     }
 
     if (currentQuiz < quizData.length-1) {
@@ -82,6 +115,4 @@ submitBtn.addEventListener("click", () => {
     } else {
         submitBtn.innerText = 'Submit'
     }
-
-
 });
